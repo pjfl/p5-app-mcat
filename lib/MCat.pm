00e5c013f3da1444cb2b351ff7267bb2dececc52
Lazarus Long <github@roxsoft.co.uk>
2023-03-14 00:14:20 +0000

    Cleaned up meta data

--- lib/MCat.pm
+++ lib/MCat.pm
@@ -1,7 +1,7 @@
 package MCat;
 
 use 5.010001;
-use version; our $VERSION = qv( sprintf '0.1.%d', q$Rev: 17 $ =~ /\d+/gmx );
+use version; our $VERSION = qv( sprintf '0.1.%d', q$Rev: 18 $ =~ /\d+/gmx );
 
 use namespace::autoclean;
 
